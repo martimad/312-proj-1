3f5f92d93ebab1e3c7be092ba2a33e081f1883c5
martimad <iliketodoart@gmail.com>
2022-09-13 20:02:51 -0600

    comments added explaining complexity

--- fermat.py
+++ fermat.py
@@ -8,63 +8,62 @@ def prime_test(N, k):
 
 
 # efficiently mods large numbers
-#
-def mod_exp(x, y, N): #x is the base case, y is the exp and N is our Mod number
-    if y == 0:  #then we have reached the base case and anything to 0 is 1
+# Time complexity O(n^3) - detailed below
+# Space complexity O(n^2) - because of recursive nature
+def mod_exp(x, y, N):  # x is the base case, y is the exp and N is our Mod number
+    if y == 0:  # then we have reached the base case and anything to 0 is 1
         return 1
-    z = mod_exp(x, floor(y/2), N) #allows us to "keep track" and recurse out once we have found if it is zero or not
+    z = mod_exp(x, floor(y / 2), N)  # allows us to "keep track" and recurse out once we have found if it is zero or not
 
     if y % 2 == 0:
-        return (z**2) % N #O(n^2)
-    return (x * z**2) % N #O(n^2)
+        return (z ** 2) % N  # O(n^2)
+    return (x * z ** 2) % N  # O(n^2)
+# repeated n times for O(n^3)
 
 
-#probability of fermat being right
+# probability of fermat being right
 def fprobability(k):
     # You will need to implement this function and change the return value.   
-    return (1-(1/(2**k)))
+    return (1 - (1 / (2 ** k)))
 
 
-#probability of millar-rabin being right
+# probability of millar-rabin being right
 def mprobability(k):
     # You will need to implement this function and change the return value.   
-    return (1-(1/(4**k)))
+    return (1 - (1 / (4 ** k)))
 
 
-#
-#O()
-def fermat(N,k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-    #
-    # To generate random values for a, you will most
-    # likely want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-    for i in range(1,k):
-        a = random.randint(1, (N-1))
-        if not mod_exp(a, (N-1), N) == 1:
+# for k iterations, chooses a random a base, and calles mod_exp to make sure that
+# it never finds anything besides 1, in which case it is probably prime
+# Time complexity - O(kn^3) - k iterations of mod_exp which is O(n^3)
+# Space complexity - O(n^2) - as much as mod_exp
+def fermat(N, k):
+    for i in range(1, k):   #O(k) iterations,
+        a = random.randint(1, (N - 1))   #O(log(n)) for rand int
+        if not mod_exp(a, (N - 1), N) == 1: #O(n^3) for mod_exp
             return 'composite'
     return 'prime'
 
 
+# tests numbers that have passed the fermat test by taking repeated square roots
+# to further weed out carmichael numbers. If the mod_exp of this square rooted
+# number does not equal one, we have found a composite
+# if the number's exponent reaches an odd power after running through
+# square root tests, or returns a value of (N-1) then we have found a prime
+# Time complexity - O(k^2n^5log(y))
+# Space complexity - O(n^2) - no more than fermat or mod_exp at one time
 def miller_rabin(N, k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-    #
-    # To generate random values for 'a', you will most likely want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-    if fermat(N, k) == 'prime':
-        for i in range(1, k):
-            a = random.randint(1, (N-1))
+    if fermat(N, k) == 'prime':  #O(kn^3)
+        for i in range(1, k):  #O(k)
+            a = random.randint(1, (N - 1)) #O(logn)
             y = N - 1
-            while y != 0:
-                b = mod_exp(a, y, N)
-                if y % 2 == 1 or b == (N-1):   #if you get an odd exponent or hit N-1 then the Miller-Rabin test stops and returns "prime"
+            while y != 0: #O(log(y))
+                b = mod_exp(a, y, N) #O(n^2)
+                if y % 2 == 1 or b == (  #O(n^2) for very bad mod cases
+                        N - 1):  # if you get an odd exponent or hit N-1 then the Miller-Rabin test stops and returns "prime"
                     return 'prime'
                 if not b == 1:
                     return 'composite'
                 else:
-                    y = y/2
-
+                    y = y / 2
+    return 'composite'
